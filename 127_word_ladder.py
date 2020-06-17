b88881298c32124e12a9614669e8e6a83d971139
sam001386 <32662528+sam001386@users.noreply.github.com>
2020-06-16 23:50:17 -0400

    Update 127_word_ladder.py

--- 127_word_ladder.py
+++ 127_word_ladder.py
@@ -5,27 +5,30 @@ class Solution:
         # Approach1: Naive BFS 
         # TLE
         '''
-        from collections import deque
-        queue = deque()
-        queue.append(beginWord)
-        seen = set()
-        seen.add(beginWord)
-        dic = {}
-        for word in wordList:
-            if word not in dic:
-                dic[word] = True
-        count = 0
-        while queue:
-            count += 1
-            size = len(queue)
-            for _ in range(size):
-                cur = queue.popleft()
-                for i in range(len(wordList)):
-                    for letter in "abcdefghijklmnopqrstuvwxyz":
-                        if cur == endWord:
-                            return count
-                        if cur[:i] + letter + cur[i + 1:] in dic and cur[:i] + letter + cur[i + 1:] not in seen:
-                            seen.add(cur[:i] + letter + cur[i + 1:])
-                            queue.append(cur[:i] + letter + cur[i + 1:])
-        return 0
+        class Solution:
+            def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+                # BFS
+                # Tricky: how to know there is only one bit difference
+                from collections import deque
+                queue = deque()
+                queue.append(beginWord)
+                dic = {}
+                for word in wordList:
+                    if word not in dic:
+                        dic[word] = True
+                print(dic)
+                count = 0
+                while queue:
+                    count += 1
+                    size = len(queue)
+                    for _ in range(size):
+                        cur = queue.popleft()
+                        for i in range(len(wordList)):
+                            for letter in "abcdefghijklmnopqrstuvwxyz":
+                                if cur == endWord:
+                                    return count
+                                if cur[:i] + letter + cur[i + 1:] in dic and dic[cur[:i] + letter + cur[i + 1:]] == True:
+                                    queue.append(cur[:i] + letter + cur[i + 1:])
+                                    dic[cur[:i] + letter + cur[i + 1:]] = False
+                return 0
         '''

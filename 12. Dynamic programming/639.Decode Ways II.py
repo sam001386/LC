45c43cd95fa580c6667a84b839b48e30c420635a
sam001386 <32662528+sam001386@users.noreply.github.com>
2021-09-26 19:46:08 -0400

    Update 639.Decode Ways II.py

--- 12. Dynamic programming/639.Decode Ways II.py
+++ 12. Dynamic programming/639.Decode Ways II.py
@@ -1,4 +1,38 @@
+# Approach 1
+class Solution(object):
+    def numDecodings(self, s):
+        dp = [0 for _ in range(len(s)+1)]
+        dp[0] = 1
+        MOD = 10 ** 9 + 7
+        for i in range(1, len(s)+1):
+            # 1-bit case
+            if s[i-1] == "*":
+                dp[i] += dp[i-1] * 9
+            elif s[i-1] != "0":
+                dp[i] += dp[i-1]
+            dp[i] %= MOD
+            # 2-bit case
+            if i >= 2:
+                if s[i-2] == "*" and s[i-1] == "*":
+                    dp[i] += dp[i-2] * 15 
+                elif s[i-2] == "*":
+                    if int(s[i-1]) <= 6:
+                        dp[i] += dp[i-2] * 2
+                    else:
+                        dp[i] += dp[i-2] 
+                elif s[i-1] == "*":
+                    if int(s[i-2]) == 1:
+                        dp[i] += dp[i-2] * 9
+                    elif int(s[i-2]) == 2:
+                        dp[i] += dp[i-2] * 6
+                elif 10 <= int(s[i-2:i]) <= 26:
+                    dp[i] += dp[i-2]
+                
+            dp[i] %= MOD
 
+        return dp[-1]
+
+# Approach 2
 # The following approach can pass all the 217/217 test cases, 
 # But still TLE in Python because of constant time limitation, 
 # The OJ should have a little larger data size for this question 
